f86a3bf488951dd1e4e10ffebd12ef1c81ed4642
Rizki Madya <rizkimadya88@gmail.com>
2023-05-11 20:19:27 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Olahraga Populer</title>
+    <link rel="shortcut icon"
+        href="https://1.bp.blogspot.com/-MZzxZbc9VMc/WrKE5ALd9tI/AAAAAAAAGKM/iS1AeX9iCo8KV62Yy3XjUFIqWHnvydPjACLcBGAs/s1600/Sepak_Bola.png"
+        type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+</head>
+
+<body>
+
+    <section id="hero">
+        <nav class="navbar navbar-expand-lg bg-transparent">
+            <div class="container">
+                <a class="navbar-brand text-light fw-semibold" href="#">
+                    <img src="https://1.bp.blogspot.com/-MZzxZbc9VMc/WrKE5ALd9tI/AAAAAAAAGKM/iS1AeX9iCo8KV62Yy3XjUFIqWHnvydPjACLcBGAs/s1600/Sepak_Bola.png"
+                        width="30px">
+                    Ronaldo.Com</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="#hero">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="#jenis-olahraga">Jenis Olahraga</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kontak</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="isi-content row mx-auto">
+                <div class="col-12">
+                    <h1>Selamat Datang di <br> Website Olahraga Terpopuler</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="jenis-olahraga">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="title">Jenis Olahraga</p>
+                    <p class="subtitle">Beberapa jenis olahraga populer di Indonesia</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card shadow overflow-hidden border-0">
+                        <img src="https://cdn.antaranews.com/cache/730x487/2022/07/08/000_326927P.jpg" alt="">
+                        <div class="p-3">
+                            <p>Nama Olahraga</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, vitae.....</p>
+                            <button class="btn btn-costum">Lihat Detail</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card shadow overflow-hidden border-0">
+                        <img src="https://cdn.antaranews.com/cache/730x487/2022/07/08/000_326927P.jpg" alt="">
+                        <div class="p-3">
+                            <p>Nama Olahraga</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, vitae.....</p>
+                            <button class="btn btn-costum">Lihat Detail</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card shadow overflow-hidden border-0">
+                        <img src="https://cdn.antaranews.com/cache/730x487/2022/07/08/000_326927P.jpg" alt="">
+                        <div class="p-3">
+                            <p>Nama Olahraga</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, vitae.....</p>
+                            <button class="btn btn-costum">Lihat Detail</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- JavaScript -->
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
